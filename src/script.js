7a1a03548cc77d3ebc63804df801187970ea3829
miranda <miranda.managadze267@cve.tsu.edu.ge>
2025-10-31 11:44:20 +0400

    create humburger menu

---
+++ src/script.js
@@ -0,0 +1,19 @@
+<script>
+  document.addEventListener("DOMContentLoaded", () => {
+    const hamburger = document.getElementById("hamburger");
+    const navList = document.getElementById("nav-list");
+
+    hamburger.addEventListener("click", () => {
+      hamburger.classList.toggle("active");
+      navList.classList.toggle("show");
+    });
+
+    // Close menu when link clicked
+    document.querySelectorAll(".nav-item").forEach((item) => {
+      item.addEventListener("click", () => {
+        hamburger.classList.remove("active");
+        navList.classList.remove("show");
+      });
+    });
+  });
+</script>
